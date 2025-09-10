86584f83b36989231d4e842068e6fcf9457dc96b
Volodymyr Voronov <malones.vladimir@gmail.com>
2025-09-10 19:39:12 +0200

    Add red color styling to error icon in ErrorBlock component

--- src/components/ErrorBlock.tsx
+++ src/components/ErrorBlock.tsx
@@ -4,7 +4,7 @@ const ErrorBlock = () => {
   return (
     <div className="flex items-center justify-center h-screen w-screen ">
       <div className="flex flex-col gap-2 py-4 px-8 bg-white dark:bg-primary rounded-full justify-center items-center text-primary border-2">
-        <TriangleAlertIcon />
+        <TriangleAlertIcon className="text-rose-500" />
         <h1 className="text-3xl font-bold">Something went wrong!</h1>
         <p className="text-lg">Please try again later.</p>
       </div>
