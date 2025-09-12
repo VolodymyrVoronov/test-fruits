19a75f00d7d37fd469b411b05c00f1b2312c73ff
Volodymyr Voronov <malones.vladimir@gmail.com>
2025-09-12 19:09:54 +0200

    Add scroll progress indicator to Main component

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,3 +1,5 @@
+import { motion, useScroll, useSpring } from "framer-motion";
+
 import useProcessFruits from "@/hooks/useProcessFruits";
 import type { IFruit } from "@/types";
 
@@ -9,18 +11,33 @@ interface IMainProps {
 }
 
 const Main = ({ fruits }: IMainProps) => {
+  const { scrollYProgress } = useScroll();
+
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 200,
+    damping: 30,
+    mass: 1,
+  });
+
   const { header, processedFruits } = useProcessFruits(fruits);
 
   return (
-    <div className=" flex flex-col gap-4">
-      {header}
+    <>
+      <motion.div
+        className="fixed top-0 left-0 right-0 h-1 bg-primary origin-left z-[9999]"
+        style={{ scaleX }}
+      />
 
-      <Fruits>
-        {processedFruits.map((processedFruit) => (
-          <Fruit key={processedFruit.id} fruit={processedFruit} />
-        ))}
-      </Fruits>
-    </div>
+      <div className=" flex flex-col gap-4">
+        {header}
+
+        <Fruits>
+          {processedFruits.map((processedFruit) => (
+            <Fruit key={processedFruit.id} fruit={processedFruit} />
+          ))}
+        </Fruits>
+      </div>
+    </>
   );
 };
 
